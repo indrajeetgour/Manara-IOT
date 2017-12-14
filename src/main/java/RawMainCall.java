4c4d2644c64033d6c89ee77834b4d68f9b0772cc
IGour <ijgour@gmail.com>
2017-12-14 11:25:29 +0530

    Check-in the Excel read part,its a first version of excel read.

--- src/main/java/RawMainCall.java
+++ src/main/java/RawMainCall.java
@@ -21,7 +21,11 @@ public class RawMainCall {
         System.out.println("\n Preprocess started :");
 //        RawFileReader.readNWriteCsvFile(fileName, fileNameOut);
 
-        RawFileReader.getAllFiles(ipPath, opPath);
+//        RawFileReader.getAllFiles(ipPath, opPath);
+
+//        RawFileReader.readFromExcel();
+
+
 //        RawFileReader.moveFilesAfterPreprocessing();
 
     }
